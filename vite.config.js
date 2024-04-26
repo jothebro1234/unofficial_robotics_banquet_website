ced2e6372520a053deeda40c1f3415437425c82c
Joseph <josephyoon25@gmail.com>
2024-04-25 19:08:19 -0700

    Deployed website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig({ 
+  base: "/{unofficial_robotics_banquet_website}", 
+  plugins: [react()], 
+}) 
 
+/*
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+*/
